8acfaad10995068a810df06228cb0dc6945bbcc3
Ss1006 <2319788370@qq.com>
2017-10-09 15:15:50 +0800

    Add files via upload

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+import logging
+from django.shortcuts import render
+from django.conf import settings
+
+logger = logging.getLogger('blog.views')
+
+
+# Create your views here.
+
+def global_setting(request):
+    return{
+        'SITE_NAME':settings.SITE_NAME,
+        'SITE_DESC':settings.SITE_DESC,
+    }
+def index(request):
+    try:
+        file = open('sss.txt','r')
+    except Exception as e:
+        logger.error(e)
+    return render(request,'index.html',locals())
